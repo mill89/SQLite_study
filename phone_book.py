0104a76378cbf54ec7a70cd3525899d1b11f1bf4
MilanN <n.milan89@gmail.com>
2023-11-09 21:50:57 +0600

    добавил функции измененния записи и удаление

--- phone_book.py
+++ phone_book.py
@@ -15,7 +15,7 @@ def create_db() -> None:
             number TEXT)''')
 
 
-def add_data_table(fio: str, phone_number: str, specialization: str = None) -> None:
+def add_data_table(fio: str, specialization: str, phone_number: str) -> None:
     # добавляем данные в таблицу
     with sq.connect(DB_NAME) as con:
         cur = con.cursor()
@@ -30,6 +30,19 @@ def get_data() -> list:
         return cur.fetchall()
 
 
+def change_data(name_col, new_value, condition=''):
+    with sq.connect(DB_NAME) as con:
+        cur = con.cursor()
+        cur.execute(f'''UPDATE {TABLE_NAME} SET {name_col}=? {condition}''',
+                    (new_value,))
+
+
+def remove_record(col, name):
+    with sq.connect(DB_NAME) as con:
+        cur = con.cursor()
+        cur.execute(f'''DELETE FROM {TABLE_NAME} WHERE {col}=?''', (name,))
+
+
 def show_bd():
     print('~ Phonebook ~\n')
     for s in get_data():  # чтение из БД
@@ -38,8 +51,13 @@ def show_bd():
 
 
 if __name__ == '__main__':
-    # create_db(DB_NAME) # созданние БД
+    # create_db()  # созданние БД
 
-    add_data_table(*map(str, input().split()))  # добавление записи в БД
+    # add_data_table(*map(str, input().split()))  # добавление записи в БД
+
+    # change_data('number', '548296994', "WHERE fio LIKE 'milan'") # изменение записив таблице
+    # change_data('specialization', 'husband', "WHERE fio LIKE 'milan'")
+
+    # remove_record('fio', '1') # удаление записи из БД
+
     show_bd()  # вывод данных
-
